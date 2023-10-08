902446912e514d2c1e3a7c09e45c0a60343880fb
Sergey <grigorevs102@gmail.com>
2023-10-08 22:19:33 +0300

    initial

--- src/shared/const/Links.ts
+++
@@ -1,5 +0,0 @@
-const TgUrl = "https://t.me/ecom_solutions_support";
-const MailAddress = "info@bez-oshibok.pro";
-const KazpostTrackUrl = "https://post.kz/services/postal";
-
-export { TgUrl, MailAddress, KazpostTrackUrl };
